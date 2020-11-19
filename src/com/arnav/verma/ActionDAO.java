78f4c06387921038348f140843ae98715cbbdc9a
arnav-verma22 <arn.verma22@gmail.com>
2020-11-19 08:40:02 +0530

    method to search and extract list of pizzas from database

--- src/com/arnav/verma/ActionDAO.java
+++ src/com/arnav/verma/ActionDAO.java
@@ -17,6 +17,26 @@ class ActionDAO
 	}
 	
 	
-	
+	public ArrayList<Pizza> Search(String type) throws SQLException 
+	{
+		ArrayList<Pizza> list = new ArrayList<Pizza>();
+		ResultSet rs = null;
+		String query = "select * from pizza where piztype = '" + type + "'";;
+		
+		if(this.con != null)
+		{
+			Statement st = con.createStatement();
+			rs = st.executeQuery(query);
+			//System.out.println(rs);
+		}
+		while(rs.next())
+		{
+			Pizza pz = new Pizza();
+			pz.setName(rs.getString("pizname"));
+			pz.setPrice(rs.getInt("price"));
+			list.add(pz);
+		}
+		return list;
+	}
 	
 }
