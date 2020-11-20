d7c94929e0e13f7540d5f9ab95dba2b1f5956e76
arnav-verma22 <arn.verma22@gmail.com>
2020-11-20 15:29:47 +0530

    login actions establishing connections

---
+++ src/com/arnav/verma/LoginDAO.java
@@ -0,0 +1,20 @@
+package com.arnav.verma;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+class LoginDAO
+{
+	private Connection con;
+
+	public LoginDAO(Connection con) 
+	{
+		this.con = con;
+	}
+	
+	
+}
