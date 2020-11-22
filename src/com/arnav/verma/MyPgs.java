bc96920daeb31d824e79b90b5d67aa9661a80ad9
arnav-verma22 <arn.verma22@gmail.com>
2020-11-22 21:50:23 +0530

    my account pgs servelet

---
+++ src/com/arnav/verma/MyPgs.java
@@ -0,0 +1,38 @@
+package com.arnav.verma;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MyPgs
+ */
+@WebServlet("/MyPgs")
+public class MyPgs extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyPgs() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
