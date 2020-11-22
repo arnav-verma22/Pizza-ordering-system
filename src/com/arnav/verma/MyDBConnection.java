a405328310123b4224b06235d344250cb6f30e48
arnav-verma22 <arn.verma22@gmail.com>
2020-11-22 21:37:24 +0530

    details

--- src/com/arnav/verma/MyDBConnection.java
+++ src/com/arnav/verma/MyDBConnection.java
@@ -10,5 +10,10 @@ import com.mysql.cj.xdevapi.Statement;
 
 public class MyDBConnection 
 {
+	static final String driver = "com.mysql.jdbc.Driver";
+	static String db = "jdbc:mysql://localhost/mydb";
+	private static String user = "root";
+	private static String pass = "22@ugust";
+	
 	
 }
