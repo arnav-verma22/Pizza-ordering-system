7a882647e42fb2c4133b78aa1fd563756dd91fd2
arnav-verma22 <arn.verma22@gmail.com>
2020-11-22 21:40:23 +0530

    login servlet

---
+++ src/com/arnav/verma/MyLoginServlet.java
@@ -0,0 +1,36 @@
+	package com.arnav.verma;
+	
+	import java.io.IOException;
+	import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+	import javax.servlet.ServletException;
+	import javax.servlet.annotation.WebServlet;
+	import javax.servlet.http.HttpServlet;
+	import javax.servlet.http.HttpServletRequest;
+	import javax.servlet.http.HttpServletResponse;
+	import javax.servlet.http.HttpSession;
+	
+	@WebServlet("/MyLoginServlet")
+	public class MyLoginServlet extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+	       
+	
+	    public MyLoginServlet() {
+	        super();
+	    }
+	
+	
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+			
+		}
+	
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			response.getWriter().append("Served at post: ").append(request.getContextPath());
+			doGet(request, response);
+		}
+	
+	}
