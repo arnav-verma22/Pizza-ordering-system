7a1c55fcf15156014cb41a0f90f34c8d06c5ffbb
arnav-verma22 <arn.verma22@gmail.com>
2020-11-22 21:43:16 +0530

    check username

--- src/com/arnav/verma/MyLoginServlet.java
+++ src/com/arnav/verma/MyLoginServlet.java
@@ -37,7 +37,15 @@ import javax.servlet.RequestDispatcher;
 			} catch (ClassNotFoundException | SQLException e) {
 				e.printStackTrace();
 			}
-			
+			LoginDAO login = new LoginDAO(con);
+			try {
+				if(login.Check(username, password))
+				{
+		
+					response.sendRedirect("MyPgs");
+					out.println("hello world !!");
+				}
+				
 		}
 	
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
