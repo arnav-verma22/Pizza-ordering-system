49d0416af93e667392cb3a77126d1bc097120cfe
arnav-verma22 <arn.verma22@gmail.com>
2020-11-22 21:37:03 +0530

    database connection

---
+++ src/com/arnav/verma/MyDBConnection.java
@@ -0,0 +1,14 @@
+package com.arnav.verma;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+import com.mysql.cj.xdevapi.Statement;
+
+
+public class MyDBConnection 
+{
+	
+}
