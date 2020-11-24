93ec7a1f9e8935d9b06206e3b17163caec567d28
arnav-verma22 <arn.verma22@gmail.com>
2020-11-24 22:51:25 +0530

    getters and setters

--- src/com/arnav/verma/Pg.java
+++ src/com/arnav/verma/Pg.java
@@ -16,6 +16,70 @@ public class Pg {
 	
 	private int seater;
 	
+	private String email;
 	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGen() {
+		return gen;
+	}
+
+	public void setGen(String gen) {
+		this.gen = gen;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getSeater() {
+		return seater;
+	}
+
+	public void setSeater(int seater) {
+		this.seater = seater;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getRoom() {
+		return room;
+	}
+
+	public void setRoom(String room) {
+		this.room = room;
+	}
 	
 }
