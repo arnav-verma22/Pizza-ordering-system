e56cb0508bb50ec2e3fb84b36f8e0b1926f85b8c
arnav-verma22 <arn.verma22@gmail.com>
2020-11-24 22:50:55 +0530

    variables

--- src/com/arnav/verma/Pg.java
+++ src/com/arnav/verma/Pg.java
@@ -2,5 +2,20 @@ package com.arnav.verma;
 
 public class Pg {
 	
+	private String name;
+	
+	private String contact;
+	
+	private String address;
+	
+	private String room;
+	
+	private String gen;
+	
+	private int price;
+	
+	private int seater;
+	
+	
 	
 }
